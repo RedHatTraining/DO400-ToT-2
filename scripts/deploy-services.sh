281d5169484a3a09780a446d72ad8ca0b723b930
Pablo Solar Vilariño <psolarvi@redhat.com>
2021-04-15 10:07:11 +0200

    Updated sample app to use GLS resources

--- scripts/deploy-services.sh
+++ scripts/deploy-services.sh
@@ -2,22 +2,22 @@
 
 # Deploy: currency service
 oc new-app --name currency \
-    https://github.com/TasteOfTraining/DO400-part-2#main \
+    https://github.com/RedHatTraining/DO400-ToT-2#main \
     --context-dir=currency \
     --strategy=docker
 
 # Deploy: history service
 oc new-app --name history \
-    https://github.com/TasteOfTraining/DO400-part-2#main \
+    https://github.com/RedHatTraining/DO400-ToT-2#main \
     --context-dir=history \
     --strategy=docker
 
 # Deploy: frontend service
 oc new-app --name frontend \
-    https://github.com/TasteOfTraining/DO400-part-2#main \
+    https://github.com/RedHatTraining/DO400-ToT-2#main \
     --context-dir=frontend \
     --strategy=docker \
-    --build-env REACT_APP_GW_ENDPOINT=http://exchange-do400-exchange-app.apps.na46.prod.nextcle.com/
+    --build-env REACT_APP_GW_ENDPOINT=http://exchange-do400-tot-2-app.apps.na46.prod.nextcle.com/
 
 # Exposing the frontend service
 oc expose svc/frontend
